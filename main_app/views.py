6fcbde5a6a35f238fb746080a20a2ee82ff1f8f9
Rexpert <liang2003147@gmail.com>
2020-07-18 10:43:16 +0800

    Initial Commit

---
+++ main_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, "main_app/base.html")
+
+
+def transaction(request):
+    return HttpResponse('Transaction')
