db8f693efb346f00f4ad55a0cf21f3847e54b12c
Rexpert <liang2003147@gmail.com>
2020-07-25 11:35:14 +0800

    added modal to home.html

--- main_app/views.py
+++ main_app/views.py
@@ -7,14 +7,17 @@ from django.http import HttpResponse
 accts = [{
     'type': 'Wallet',
     'details': [{
+        'id': 1,
         'name': 'TNG',
         'date': '23 Jun 2020',
         'bal': '32.58',
     }, {
+        'id': 2,
         'name': 'Cash',
         'date': '24 Jun 2020',
         'bal': '4.20',
     }, {
+        'id': 3,
         'name': 'Some Wallet',
         'date': '24 Jun 2020',
         'bal': '228.88',
@@ -22,6 +25,7 @@ accts = [{
 }, {
     'type': 'Moneybox',
     'details': [{
+        'id': 4,
         'name': 'Milo Tin',
         'date': '22 Jun 2020',
         'bal': '30.50',
@@ -29,18 +33,22 @@ accts = [{
 }, {
     'type': 'Bank',
     'details': [{
+        'id': 5,
         'name': 'Bank Islam',
         'date': '18 Jun 2020',
         'bal': '16.34',
     }, {
+        'id': 6,
         'name': 'Bank Rakyat',
         'date': '18 Jun 2020',
         'bal': '220.59',
     }, {
+        'id': 7,
         'name': 'Bank ZZZZZZZZZZ',
         'date': '18 Jun 2020',
         'bal': '888888.88',
     }, {
+        'id': 8,
         'name': 'Bank WTF',
         'date': '18 Jun 2020',
         'bal': '23232.22',
@@ -49,8 +57,12 @@ accts = [{
 
 
 def home(request):
+    if request.method == 'POST':
+        print('acct id: ', request.POST.get('id'))
+        print('amount: ', float(request.POST.get('atm_new_bal')) -
+              float(request.POST.get('bal')))
     context = {
-        'accts': accts
+        'accts': accts,
     }
     return render(request, 'main_app/home.html', context)
 
